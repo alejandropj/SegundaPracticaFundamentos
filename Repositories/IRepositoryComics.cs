cf51a61b6a69a17d7bf2c96dc350a4e3ccc5010f
alejandropj <alejandroparrajimenez@gmail.com>
2024-02-16 11:31:22 +0100

    11:30

--- Repositories/IRepositoryComics.cs
+++ Repositories/IRepositoryComics.cs
@@ -5,13 +5,9 @@ namespace SegundaPracticaFundamentos.Repositories
     public interface IRepositoryComics
     {
         List<Comic> GetComics();
-        List<string> GetAllComicsString();
-
         Comic GetComic(int idcomic);
         void InsertComicLambda(Comic comic);
         void InsertComicProcedure(Comic comic);
-
-        void UpdateComic(Comic comic); 
         void DeleteComic(int idcomic);
     }
 }
--- Repositories/RepositoryComicsOracle.cs
+++ Repositories/RepositoryComicsOracle.cs
@@ -1,42 +1,48 @@
-﻿using SegundaPracticaFundamentos.Models;
-using System.Data.SqlClient;
-using System.Data;
-using Microsoft.AspNetCore.Http.HttpResults;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Oracle.ManagedDataAccess.Client;
+using SegundaPracticaFundamentos.Models;
 using System.Collections.Generic;
+using System;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SegundaPracticaFundamentos.Repositories
 {
     #region PROCEDIMIENTO CREAR
-    /*    
-    create procedure SP_CREATE_COMIC
-    (@nombre NVARCHAR(150), @imagen NVARCHAR(600), @descripcion NVARCHAR(500)) 
-    as 
-        DECLARE @IDCOMIC INT
-        SELECT @IDCOMIC = MAX(IDCOMIC) FROM COMICS
-        SET @IDCOMIC = @IDCOMIC + 1
+    /*        
+     create or replace procedure SP_CREATE_COMIC
+    (sp_nombre COMICS.NOMBRE%TYPE, sp_imagen COMICS.IMAGEN%TYPE,
+    sp_descripcion COMICS.DESCRIPCION%TYPE)
+    IS
+       v_idcomic INTEGER;
+    BEGIN
+       SELECT MAX(IDCOMIC) INTO v_idcomic FROM COMICS;
+            v_idcomic := v_idcomic + 1;
 
 
-        insert into COMICS values(@IDCOMIC, @nombre, @imagen, @descripcion)
-    go*/
+       insert into COMICS values(v_idcomic, sp_nombre, sp_imagen
+       , sp_descripcion);
+            commit;
+    end;*/
     #endregion
-    public class RepositoryComicsSQLServer : IRepositoryComics
+    public class RepositoryComicsOracle:IRepositoryComics
     {
         private DataTable tabla;
-        private SqlConnection cn;
-        private SqlCommand com;
+        private OracleConnection cn;
+        private OracleCommand com;
 
-        public RepositoryComicsSQLServer()
+        public RepositoryComicsOracle()
         {
-            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=sa;Password=MCSD2023";
-            this.cn = new SqlConnection(connectionString);
-            this.com = new SqlCommand();
+            string connectionString = @"Data Source=LOCALHOST:1521/XE;Persist Security Info=True;User Id=SYSTEM;Password=oracle";
+            this.cn = new OracleConnection(connectionString);
+            this.com = new OracleCommand();
             this.com.Connection = this.cn;
             this.tabla = new DataTable();
             string sql = "select * from COMICS";
-            SqlDataAdapter ad = new SqlDataAdapter(sql, this.cn);
+            OracleDataAdapter ad = new OracleDataAdapter(sql, this.cn);
             ad.Fill(this.tabla);
         }
+
         public List<Comic> GetComics()
         {
             var consulta = from datos in this.tabla.AsEnumerable()
@@ -77,11 +83,15 @@ namespace SegundaPracticaFundamentos.Repositories
             var consulta = from datos in this.tabla.AsEnumerable()
                            select datos.Field<int>("IDCOMIC");
             max = consulta.Max(x => x) + 1;
-            string sql = "insert into COMICS values (@idcomic,@nombre,@imagen,@descripcion)";
-            this.com.Parameters.AddWithValue("@idcomic", max);
-            this.com.Parameters.AddWithValue("@nombre", comic.Nombre);
-            this.com.Parameters.AddWithValue("@imagen", comic.Imagen);
-            this.com.Parameters.AddWithValue("@descripcion", comic.Descripcion);
+            string sql = "insert into COMICS values (:idcomic,:nombre,:imagen,:descripcion)";
+            OracleParameter pamId = new OracleParameter(":idcomic", max);
+            this.com.Parameters.Add(pamId);
+            OracleParameter pamNombre = new OracleParameter(":nombre", comic.Nombre);
+            this.com.Parameters.Add(pamNombre);
+            OracleParameter pamImagen = new OracleParameter(":imagen", comic.Imagen);
+            this.com.Parameters.Add(pamImagen);
+            OracleParameter pamDescripcion = new OracleParameter(":descripcion", comic.Descripcion);
+            this.com.Parameters.Add(pamDescripcion);
             this.com.CommandType = CommandType.Text;
             this.com.CommandText = sql;
             this.cn.Open();
@@ -91,9 +101,12 @@ namespace SegundaPracticaFundamentos.Repositories
         }
         public void InsertComicProcedure(Comic comic)
         {
-            this.com.Parameters.AddWithValue("@nombre", comic.Nombre);
-            this.com.Parameters.AddWithValue("@imagen", comic.Imagen);
-            this.com.Parameters.AddWithValue("@descripcion", comic.Descripcion);
+            OracleParameter pamNombre = new OracleParameter(":sp_nombre", comic.Nombre);
+            this.com.Parameters.Add(pamNombre);
+            OracleParameter pamImagen = new OracleParameter(":sp_imagen", comic.Imagen);
+            this.com.Parameters.Add(pamImagen);
+            OracleParameter pamDescripcion = new OracleParameter(":sp_descripcion", comic.Descripcion);
+            this.com.Parameters.Add(pamDescripcion);
             this.com.CommandType = CommandType.StoredProcedure;
             this.com.CommandText = "SP_CREATE_COMIC";
             this.cn.Open();
@@ -104,8 +117,9 @@ namespace SegundaPracticaFundamentos.Repositories
 
         public void DeleteComic(int idcomic)
         {
-            string sql = "delete from COMICS where IDCOMIC=@idcomic";
-            this.com.Parameters.AddWithValue("@idcomic", idcomic);
+            string sql = "delete from COMICS where IDCOMIC=:idcomic";
+            OracleParameter pamId = new OracleParameter(":idcomic", idcomic);
+            this.com.Parameters.Add(pamId);
             this.com.CommandType = CommandType.Text;
             this.com.CommandText = sql;
             this.cn.Open();
